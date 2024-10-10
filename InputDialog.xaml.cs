1bdbbb7ce9c6bbc5be0091a5c3e1573259f4215f
m1targ1 <sdm.m1targ1@gmail.com>
2024-10-10 14:47:29 +0300

    Добавьте файлы проекта.

---
+++ InputDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Mod_4_V3_
+{
+    public partial class InputDialog : Window
+    {
+        public string InputText { get; private set; }
+
+        public InputDialog(string prompt)
+        {
+            InitializeComponent();
+            PromptTextBlock.Text = prompt;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputText = InputTextBox.Text;
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
